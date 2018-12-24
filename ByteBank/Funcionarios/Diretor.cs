d25bc139950e62a9234c264bd5a8c26f300a9958
Sleepk <julio.cesar.vgs@gmail.com>
2018-12-24 20:40:54 -0200

    Capitulo 2, Heranca

--- ByteBank/Funcionarios/Diretor.cs
+++ ByteBank/Funcionarios/Diretor.cs
@@ -8,10 +8,9 @@ namespace ByteBank.Funcionarios
 {
     public class Diretor : Funcionario
     {
-        
-        public double GetBonificacao()
+        public override double GetBonificacao()
         {
-            return Salario;
+            return Salario + base.GetBonificacao();
         }
     }
 }
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,5 +1,9 @@
 ﻿using ByteBank.Funcionarios;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ByteBank
 {
@@ -15,11 +19,17 @@ namespace ByteBank
             carlos.Salario = 2000;
 
             gerenciador.Registrar(carlos);
-
+            
             Diretor roberta = new Diretor();
             roberta.Nome = "Roberta";
             roberta.CPF = "454.658.148-3";
             roberta.Salario = 5000;
+
+            Funcionario robertaTeste = roberta;
+
+            Console.WriteLine("Bonificacao de uma referencia de Diretor: " + roberta.GetBonificacao());
+            Console.WriteLine("Bonificacao de uma referencia de Funcionario: " + robertaTeste.GetBonificacao());
+
 
             gerenciador.Registrar(roberta);
 
